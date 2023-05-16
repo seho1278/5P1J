f764eb26702a86c120a8e9e5c08b39fb861ceef7
mungjimangji <mungjmangji@hphk.com>
2023-05-16 10:02:31 +0900

    movies_forms.py update

--- movies/forms.py
+++ movies/forms.py
@@ -66,11 +66,7 @@ class ReviewForm(forms.ModelForm):
         choices = TAG_CHOICES,
 
     )
-    # rating = forms.ChoiceField(choices=[(1, '⭐'), (2, '⭐⭐'), (3, '⭐⭐⭐'), (4, '⭐⭐⭐⭐'), (5, '⭐⭐⭐⭐⭐')], widget=forms.RadioSelect(
-    # attrs= {
-    #     'class':'rating-horizontal'
-    # }
-    # ), required=False, label = '',)
+
 
     class Meta:
         model = Review
